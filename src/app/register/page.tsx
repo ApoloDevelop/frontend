fd86e050b650a638512c1e19d4a9cee59ac5a2ac
Pablo Robles <pablormans@gmail.com>
2025-05-12 14:09:40 +0200

    second page (countries)

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,7 +1,11 @@
 "use client";
 
-import { useState } from "react";
+import { FC, use, useState } from "react";
 import { Button } from "@/components/ui/button";
+import { countries } from "@/data/countries";
+import Flag from "react-world-flags";
+import Select from "react-select";
+import { SortAsc } from "lucide-react";
 
 export default function RegisterPage() {
   const [step, setStep] = useState(1); // Controla el paso actual del slide
@@ -10,6 +14,11 @@ export default function RegisterPage() {
     username: "",
     email: "",
     password: "",
+    birthdate: "",
+    country: "",
+    city: "",
+    postalCode: "",
+    phone: "",
   });
 
   const handleNext = () => {
@@ -23,6 +32,40 @@ export default function RegisterPage() {
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
+
+  // const handleCountryChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+  //   setFormData({ ...formData, country: e.target.value });
+  // };
+
+  const handleCountryChange = (selectedOption: any) => {
+    setFormData({
+      ...formData,
+      country: selectedOption?.value || "",
+    });
+  };
+
+  const handleDialCodeChange = (selectedOption: any) => {
+    setFormData({
+      ...formData,
+      phone: selectedOption?.value || "",
+    });
+  };
+
+  const countryOptions = countries
+    .sort((a, b) =>
+      a.translations.spa.common.localeCompare(b.translations.spa.common, "es")
+    ) // Ordena los países por su traducción al español
+    .map((country) => ({
+      value: country.translations.spa.common,
+      SortAsc: country.translations.spa.common,
+      label: (
+        <div className="flex items-center">
+          <Flag code={country.cca2} className="w-5 h-5 mr-2" />
+          {country.translations.spa.common}
+        </div>
+      ),
+      dialCode: country.idd.root + (country.idd.suffixes?.[0] || ""),
+    }));
 
   return (
     <div
@@ -45,9 +88,9 @@ export default function RegisterPage() {
         }}
       ></div>
       <div
-        className="p-8 rounded-xl w-1/3 max-w-md min-h-[500px] transform transition flex flex-col gap-y-px duration-300 hover:scale-103 hover:shadow-3xl opacity-95 animate-fade-in"
+        className="p-3 rounded-xl w-1/3 max-w-md min-h-[500px] transform transition flex flex-col gap-y-px duration-300 hover:scale-103 hover:shadow-3xl opacity-95 animate-fade-in"
         style={{
-          backgroundColor: "var(--container-background);",
+          backgroundColor: "var(--container-background)",
           boxShadow: "0 4px 50px 20px rgba(0, 0, 0, 0.5)",
         }}
       >
@@ -62,9 +105,9 @@ export default function RegisterPage() {
             style={{ transform: `translateX(-${(step - 1) * 100}%)` }}
           >
             {/* Página 1 */}
-            <div className="w-full flex-shrink-0">
+            <div className="w-full flex-shrink-0 ">
               <h3 className="text-lg mb-4 text-center">Háblanos de ti</h3>
-              <form className="space-y-4">
+              <form className="space-y-4 flex flex-col items-center">
                 <input
                   type="text"
                   name="fullname"
@@ -72,7 +115,7 @@ export default function RegisterPage() {
                   value={formData.fullname}
                   onChange={handleChange}
                   required
-                  className="w-full px-3 py-2 border rounded mb-8 border-black"
+                  className="w-9/10 px-3 py-2 border rounded mb-8 border-black"
                 />
                 <input
                   type="text"
@@ -81,7 +124,7 @@ export default function RegisterPage() {
                   value={formData.username}
                   onChange={handleChange}
                   required
-                  className="w-full px-3 py-2 border rounded mb-8 border-black"
+                  className="w-9/10 px-3 py-2 border rounded mb-8 border-black"
                 />
                 <input
                   type="email"
@@ -90,7 +133,7 @@ export default function RegisterPage() {
                   value={formData.email}
                   onChange={handleChange}
                   required
-                  className="w-full px-3 py-2 border rounded mb-8 border-black"
+                  className="w-9/10 px-3 py-2 border rounded mb-8 border-black"
                 />
                 <input
                   type="password"
@@ -99,15 +142,107 @@ export default function RegisterPage() {
                   value={formData.password}
                   onChange={handleChange}
                   required
-                  className="w-full px-3 py-2 border rounded mb-8 border-black"
+                  className="w-9/10 px-3 py-2 border rounded mb-8 border-black"
                 />
               </form>
             </div>
 
             {/* Placeholder para futuras páginas */}
             <div className="w-full flex-shrink-0">
-              <h3 className="text-lg mb-4">Página 2 (Placeholder)</h3>
-              <p>Contenido de la segunda página.</p>
+              <h3 className="text-lg mb-4 text-center">¡Cuéntanos más!</h3>
+              <form className="space-y-4 flex flex-col items-center">
+                <input
+                  type="date"
+                  max={new Date().toISOString().split("T")[0]} // Limita la fecha a hoy
+                  name="birthdate"
+                  placeholder="Fecha de nacimiento"
+                  value={formData.birthdate || ""}
+                  onChange={handleChange}
+                  required
+                  className="w-9/10 px-3 py-2 border rounded mb-8 border-black"
+                />
+                <Select
+                  options={countryOptions}
+                  value={
+                    countryOptions.find(
+                      (option) => option.value === formData.country
+                    ) || null
+                  }
+                  isClearable
+                  onChange={handleCountryChange}
+                  placeholder={"Selecciona tu país"}
+                  className={"w-9/10 mb-8"}
+                  styles={{
+                    control: (base, state) => ({
+                      ...base,
+                      borderColor: state.isFocused ? "black" : "black", // Color del borde
+                      boxShadow: state.isFocused ? "0 0 0 0px black" : "none", // Sombra al enfocar
+                      "&:hover": {
+                        borderColor: "black", // Color del borde al pasar el mouse
+                      },
+                    }),
+                  }}
+                />
+                <input
+                  type="text"
+                  name="city"
+                  placeholder="Ciudad"
+                  value={formData.city || ""}
+                  onChange={handleChange}
+                  required
+                  className="w-9/10 px-3 py-2 border rounded mb-8 border-black"
+                />
+                <input
+                  type="text"
+                  name="postalCode"
+                  placeholder="Código postal"
+                  value={formData.postalCode || ""}
+                  onChange={handleChange}
+                  required
+                  className="w-9/10 px-3 py-2 border rounded mb-8 border-black"
+                />
+                <div className="flex flex-row">
+                  <Select
+                    options={countryOptions.map((country) => ({
+                      value: country.dialCode,
+                      label: country.label,
+                    }))}
+                    isClearable
+                    onChange={(selectedOption: any) => {
+                      setFormData({
+                        ...formData,
+                        phone: selectedOption?.value || "", // Actualiza el campo de teléfono con el prefijo
+                      });
+                    }}
+                    placeholder={"Prefijo"}
+                    className={"w-1/4 mb-8 px-2"}
+                    menuPlacement="auto"
+                    styles={{
+                      control: (base, state) => ({
+                        ...base,
+                        borderColor: state.isFocused ? "black" : "black", // Color del borde
+                        boxShadow: state.isFocused ? "0 0 0 0px black" : "none", // Sombra al enfocar
+                        "&:hover": {
+                          borderColor: "black", // Color del borde al pasar el mouse
+                        },
+                      }),
+                      menu: (base) => ({
+                        ...base,
+                        width: "200px", // Ajusta el ancho del menú desplegable
+                      }),
+                    }}
+                  />
+                  <input
+                    type="tel"
+                    name="phone"
+                    placeholder="Teléfono"
+                    value={formData.phone || ""}
+                    onChange={handleChange}
+                    required
+                    className="w-9/10 px-3 py-2 border rounded mb-8 border-black"
+                  />
+                </div>
+              </form>
             </div>
 
             <div className="w-full flex-shrink-0">
