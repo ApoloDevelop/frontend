cd3732bba26a9d5b0b9f37c1882ce56ba2757f1f
Pablo Robles <pablormans@gmail.com>
2026-07-09 13:28:55 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,7 @@ const nextConfig: NextConfig = {
     domains: [
       "res.cloudinary.com",
       "i.scdn.co",
+      "is1-ssl.mzstatic.com",
       "i.imgur.com",
       "lastfm.freetls.fastly.net",
       "lh3.googleusercontent.com",
